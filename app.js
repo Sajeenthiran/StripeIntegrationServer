53f0771bb70f8fed2381820521ba0e38341113f1
Sajeenthiran <sajeebilla@gmail.com>
2020-06-09 11:03:28 +0530

    change api route

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 const { resolve } = require("path");
 // This is your real test secret API key.
 const stripe = require("stripe")("sk_test_a2LZDwzINdKtCd2BMdo21swn00Mqm40FPi");
-//app.use(express.static("."));
-//app.use(express.json());
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -12,7 +11,6 @@ app.use(bodyParser.raw());
 
 app.post("/payment/create-payment-intent", async (req, res) => {
     //const { items } = req.body;
-    // Create a PaymentIntent with the order amount and currency
     console.log("Welcome ",req.body);
     const paymentIntent = await stripe.paymentIntents.create({
         amount: 1400,
